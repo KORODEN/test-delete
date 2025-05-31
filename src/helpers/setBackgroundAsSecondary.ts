ae42724158c29fee2527d9665a8cf7bd1bffd239
koroden <korogodin.denis@mail.ru>
2025-05-31 16:11:33 +0700

    Add some parts

---
+++ src/helpers/setBackgroundAsSecondary.ts
@@ -0,0 +1,11 @@
+import { getTelegramData } from '@telegram-apps/telegram-ui/dist/helpers/telegram';
+
+export const setBackgroundAsSecondary = () => {
+  const telegramData = getTelegramData();
+  if (!telegramData) {
+    return;
+  }
+
+  telegramData.setBackgroundColor(telegramData.themeParams.secondary_bg_color);
+  telegramData.expand()
+};
